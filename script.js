4737067557f4b5b5ddba2e805ab69421a81e1949
Meiling <happylive_2006@hotmail.com>
2022-12-14 23:03:35 -0600

    update

--- script.js
+++ script.js
@@ -12,31 +12,35 @@
 // window.addEventListener("load", function() {
 
 window.addEventListener("load", function() {
-    const form = document.querySelector("form");
+
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        let selectedPlanet = pickPlanet(listedPlanets);
+         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+
+        addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);  
+    })
+    
+    let list = document.getElementById("faultyItems");
+    list.style.visibility = "hidden";
+    let = document.querySelector("form");
+ 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
-       
         let pilot = document.querySelector("input[name=pilotName]").value;
         let copilot = document.querySelector("input[name=copilotName]").value;
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
         let cargoLevel = document.querySelector("input[name=cargoMass]").value;
         let list = document.getElementById('faultyItems');
-        list.style.visibility = "hidden";
-        formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
-    })
-
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse = myFetch();
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       let selectedPlanet = pickPlanet(listedPlanets);
-       addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.image, selectedPlanet.moons);
-    })
-   
-});
+ 
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    });
+ });
 
