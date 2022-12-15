d40d3051774f5fd5033998a3466544522b6848db
Meiling <happylive_2006@hotmail.com>
2022-12-14 18:14:56 -0600

    update

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 // Write your JavaScript code here!
-// window.addEventListener("load", function() {
 
-//     let listedPlanets;
-//     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-//     let listedPlanetsResponse;
-//     listedPlanetsResponse.then(function (result) {
-//         listedPlanets = result;
-//         console.log(listedPlanets);
-//     }).then(function () {
-//         console.log(listedPlanets);
-//         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-//     })
-    
-//  });
 
 // window.addEventListener("load", function() {
 //     const form = document.querySelector("form");
@@ -64,25 +51,25 @@ window.addEventListener("load", function() {
         addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);  
     })
     
-    let list = window.document.getElementById("faultyItems");
+    let list = document.getElementById("faultyItems");
     list.style.visibility = "hidden";
     let form = document.querySelector("form");
  
     form.addEventListener("submit", function(event) {
         event.preventDefault();
         let pilotInput = document.querySelector("input[name=pilotName]");
-        let pilotName = pilotInput.value;
+        let pilot = pilotInput.value;
  
         let copilotInput = document.querySelector("input[name=copilotName]");
-        let copilotName = copilotInput.value;
+        let copilot = copilotInput.value;
  
         let fuelInput = document.querySelector("input[name=fuelLevel]");
-        let fuelLevel = Number(fuelInput.value);
+        let fuelLevel = fuelInput.value;
  
         let cargoInput = document.querySelector("input[name=cargoMass]");
-        let cargoMassKg = Number(cargoInput.value);
+        let cargoLevel = cargoInput.value;
  
-        formSubmission(document, faultyItems, pilotName.value, copilotName.value, fuelLevel.value, cargoMassKg.value);
+        formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
     });
  });
 
