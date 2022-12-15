214ddb63ea394f97230c5ee02db4896d5cbd3c51
Meiling <happylive_2006@hotmail.com>
2022-12-14 22:54:08 -0600

    update

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       
+       let selectedPlanet = pickPlanet(listedPlanets);
        addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.image, selectedPlanet.moons);
     })
    
